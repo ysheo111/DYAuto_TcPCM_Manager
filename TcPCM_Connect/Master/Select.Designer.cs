bc1d0e412476d7db5463f18633f80875a3a33cba
moonkh-yc <moonkh@ycdis.co.kr>
2024-12-18 14:01:23 +0900

    1.상세 검색 기능 1-1.콤보 박스 선택Item에 따라 달력 날짜 변경.
    2.업종
    2-1.검색 추가
    2-2.저장 추가
    2-2.엑셀 Import
    2-3.엑셀 Export

--- TcPCM_Connect/Master/Select.Designer.cs
+++ TcPCM_Connect/Master/Select.Designer.cs
@@ -33,9 +33,10 @@
             this.btnClose = new System.Windows.Forms.Button();
             this.panelBody = new System.Windows.Forms.Panel();
             this.panel_Select = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rjDatePicker2 = new CustomControls.RJControls.RJDatePicker();
             this.combo_date = new CustomControls.RJControls.RJComboBox();
             this.rjDatePicker1 = new CustomControls.RJControls.RJDatePicker();
-            this.label1 = new System.Windows.Forms.Label();
             this.btn_Cancel = new CustomControls.RJControls.RJButton();
             this.btn_Create = new CustomControls.RJControls.RJButton();
             this.panelTitleBar.SuspendLayout();
@@ -50,8 +51,9 @@
             this.panelTitleBar.Controls.Add(this.btnClose);
             this.panelTitleBar.Dock = System.Windows.Forms.DockStyle.Top;
             this.panelTitleBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTitleBar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelTitleBar.Name = "panelTitleBar";
-            this.panelTitleBar.Size = new System.Drawing.Size(595, 38);
+            this.panelTitleBar.Size = new System.Drawing.Size(788, 48);
             this.panelTitleBar.TabIndex = 3;
             this.panelTitleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelTitleBar_MouseDown);
             // 
@@ -60,9 +62,9 @@
             this.labelCaption.AutoSize = true;
             this.labelCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.labelCaption.ForeColor = System.Drawing.Color.White;
-            this.labelCaption.Location = new System.Drawing.Point(10, 7);
+            this.labelCaption.Location = new System.Drawing.Point(11, 9);
             this.labelCaption.Name = "labelCaption";
-            this.labelCaption.Size = new System.Drawing.Size(85, 20);
+            this.labelCaption.Size = new System.Drawing.Size(107, 25);
             this.labelCaption.TabIndex = 4;
             this.labelCaption.Text = "조회조건 설정";
             // 
@@ -74,9 +76,10 @@
             this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnClose.ForeColor = System.Drawing.Color.White;
-            this.btnClose.Location = new System.Drawing.Point(548, 0);
+            this.btnClose.Location = new System.Drawing.Point(734, 0);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnClose.Name = "btnClose";
-            this.btnClose.Size = new System.Drawing.Size(47, 38);
+            this.btnClose.Size = new System.Drawing.Size(54, 48);
             this.btnClose.TabIndex = 3;
             this.btnClose.Text = "X";
             this.btnClose.UseVisualStyleBackColor = false;
@@ -90,9 +93,10 @@
             this.panelBody.Controls.Add(this.btn_Create);
             this.panelBody.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelBody.Location = new System.Drawing.Point(0, 0);
+            this.panelBody.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panelBody.Name = "panelBody";
-            this.panelBody.Padding = new System.Windows.Forms.Padding(12, 9, 0, 0);
-            this.panelBody.Size = new System.Drawing.Size(595, 316);
+            this.panelBody.Padding = new System.Windows.Forms.Padding(14, 11, 0, 0);
+            this.panelBody.Size = new System.Drawing.Size(788, 388);
             this.panelBody.TabIndex = 5;
             // 
             // panel_Select
@@ -100,14 +104,38 @@
             this.panel_Select.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel_Select.Controls.Add(this.rjDatePicker2);
             this.panel_Select.Controls.Add(this.combo_date);
             this.panel_Select.Controls.Add(this.rjDatePicker1);
             this.panel_Select.Controls.Add(this.label1);
-            this.panel_Select.Location = new System.Drawing.Point(10, 47);
+            this.panel_Select.Location = new System.Drawing.Point(11, 59);
             this.panel_Select.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel_Select.Name = "panel_Select";
-            this.panel_Select.Size = new System.Drawing.Size(574, 218);
+            this.panel_Select.Size = new System.Drawing.Size(764, 265);
             this.panel_Select.TabIndex = 65;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "검색기간";
+            // 
+            // rjDatePicker2
+            // 
+            this.rjDatePicker2.BorderColor = System.Drawing.Color.Black;
+            this.rjDatePicker2.BorderSize = 1;
+            this.rjDatePicker2.Font = new System.Drawing.Font("굴림", 9.5F);
+            this.rjDatePicker2.Location = new System.Drawing.Point(486, 12);
+            this.rjDatePicker2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.rjDatePicker2.MinimumSize = new System.Drawing.Size(4, 35);
+            this.rjDatePicker2.Name = "rjDatePicker2";
+            this.rjDatePicker2.Size = new System.Drawing.Size(235, 35);
+            this.rjDatePicker2.SkinColor = System.Drawing.Color.White;
+            this.rjDatePicker2.TabIndex = 2;
+            this.rjDatePicker2.TextColor = System.Drawing.Color.Black;
             // 
             // combo_date
             // 
@@ -127,36 +155,28 @@
             "1년"});
             this.combo_date.ListBackColor = System.Drawing.Color.White;
             this.combo_date.ListTextColor = System.Drawing.Color.DimGray;
-            this.combo_date.Location = new System.Drawing.Point(93, 10);
+            this.combo_date.Location = new System.Drawing.Point(106, 12);
             this.combo_date.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.combo_date.Name = "combo_date";
             this.combo_date.Padding = new System.Windows.Forms.Padding(1);
-            this.combo_date.Size = new System.Drawing.Size(97, 28);
+            this.combo_date.Size = new System.Drawing.Size(111, 35);
             this.combo_date.TabIndex = 0;
             this.combo_date.Texts = "오늘";
+            this.combo_date.OnSelectedIndexChanged += new System.EventHandler(this.combo_date_OnSelectedIndexChanged);
             // 
             // rjDatePicker1
             // 
-            this.rjDatePicker1.BorderColor = System.Drawing.Color.PaleVioletRed;
-            this.rjDatePicker1.BorderSize = 0;
+            this.rjDatePicker1.BorderColor = System.Drawing.Color.Black;
+            this.rjDatePicker1.BorderSize = 1;
             this.rjDatePicker1.Font = new System.Drawing.Font("굴림", 9.5F);
-            this.rjDatePicker1.Location = new System.Drawing.Point(207, 10);
+            this.rjDatePicker1.Location = new System.Drawing.Point(245, 12);
             this.rjDatePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rjDatePicker1.MinimumSize = new System.Drawing.Size(4, 35);
             this.rjDatePicker1.Name = "rjDatePicker1";
-            this.rjDatePicker1.Size = new System.Drawing.Size(226, 35);
-            this.rjDatePicker1.SkinColor = System.Drawing.Color.MediumSlateBlue;
+            this.rjDatePicker1.Size = new System.Drawing.Size(235, 35);
+            this.rjDatePicker1.SkinColor = System.Drawing.Color.White;
             this.rjDatePicker1.TabIndex = 1;
-            this.rjDatePicker1.TextColor = System.Drawing.Color.White;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 18);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(53, 12);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "검색기간";
+            this.rjDatePicker1.TextColor = System.Drawing.Color.Black;
             // 
             // btn_Cancel
             // 
@@ -171,9 +191,10 @@
             this.btn_Cancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btn_Cancel.ForeColor = System.Drawing.Color.Crimson;
-            this.btn_Cancel.Location = new System.Drawing.Point(515, 274);
+            this.btn_Cancel.Location = new System.Drawing.Point(697, 335);
+            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Cancel.Name = "btn_Cancel";
-            this.btn_Cancel.Size = new System.Drawing.Size(62, 31);
+            this.btn_Cancel.Size = new System.Drawing.Size(71, 39);
             this.btn_Cancel.TabIndex = 64;
             this.btn_Cancel.Text = "취소";
             this.btn_Cancel.TextColor = System.Drawing.Color.Crimson;
@@ -193,9 +214,10 @@
             this.btn_Create.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Create.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.btn_Create.ForeColor = System.Drawing.Color.DodgerBlue;
-            this.btn_Create.Location = new System.Drawing.Point(448, 274);
+            this.btn_Create.Location = new System.Drawing.Point(620, 335);
+            this.btn_Create.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btn_Create.Name = "btn_Create";
-            this.btn_Create.Size = new System.Drawing.Size(62, 31);
+            this.btn_Create.Size = new System.Drawing.Size(71, 39);
             this.btn_Create.TabIndex = 63;
             this.btn_Create.Text = "조회";
             this.btn_Create.TextColor = System.Drawing.Color.DodgerBlue;
@@ -204,9 +226,9 @@
             // 
             // Select
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(595, 316);
+            this.ClientSize = new System.Drawing.Size(788, 388);
             this.Controls.Add(this.panelTitleBar);
             this.Controls.Add(this.panelBody);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -235,5 +257,6 @@
         private System.Windows.Forms.Label label1;
         private CustomControls.RJControls.RJDatePicker rjDatePicker1;
         private CustomControls.RJControls.RJComboBox combo_date;
+        private CustomControls.RJControls.RJDatePicker rjDatePicker2;
     }
 }
